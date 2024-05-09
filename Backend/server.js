9ff6fc9203e058234b8220484ad9b1e8fc2fec24
Victor Zhou <victor@Victors-MacBook-Air-3.local>
2024-05-08 23:08:34 -0400

    latest function add

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,44 @@ import mongoose from 'mongoose';
 
 const uri = 'mongodb+srv://vz2164:Thecodeis1@cluster0.vqhcgvm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const app = express();
+let countID = 0;
+
+app.use(express.json());
 
 mongoose.connect(uri)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB connection error:', err));
+
+const Note = mongoose.model('Note', new mongoose.Schema({ _id: Number, title: String, content: String}, { _id: false }));
+
+app.get('/notes', async (req, res) => {
+    try {
+        const notes = await Note.find({});
+        res.json(notes);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post('/notes', async (req, res) => {
+    try {
+        const newNote = new Note({ _id: countID++, title: req.body.title, content: req.body.content });
+        await newNote.save();
+        res.json({ message: 'Successfully added' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.delete('/notes/:id', async (req, res) => {
+    try {
+        console.log(req.params.id)
+        await Note.findOneAndDelete({ _id: req.params.id });
+        res.send('Successfully deleted');
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 app.listen(8000, () => {
     console.log("Server started on port 8000")
--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from "react";
+import Axios from 'axios'
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
-import noteItems from "./notes.js";
 
 function App() {
-  const [notes, setNotes] = useState(noteItems);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    Axios.get('/notes').then(response => {
+      console.log('Response:', response.data);
+      setNotes(response.data);
+    }).catch(error => {
+      console.error('Error fetching notes:', error);
+    });
+  }, []);
 
   const addNote = (newNote) => {
+    Axios.post('/notes', newNote).then(response => {
+      console.log('Response:', response.data);
+    }).catch(error => {
+      console.error('Error adding a new note:', error);
+    });
     setNotes((prevNotes) => [...prevNotes, newNote]);
   };
 
   const deleteNote = (id) => {
+    Axios.delete(`/notes/`+id).then(response => {
+      console.log('Response:', response.data);
+    }).catch(error => {
+      console.error('Error deleting note:', error);
+    });
     setNotes((prevNotes) =>
       prevNotes.filter((noteItem, index) => index !== id)
     );
